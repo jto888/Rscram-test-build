ded98de8cda859802e565b68a0b1c28b10acb2fc
Jacob Ormerod <jtormerod@gmail.com>
2017-05-30 17:22:03 -0400

    build6 to confirm header only for conditional.h

--- Rscram/src/attribute_test.cpp
+++ Rscram/src/attribute_test.cpp
@@ -1,11 +1,13 @@
 #include "Rscram.h"
 #include "element.h"
 
-// these includes were used to drive test build of header only expression sources
-//#include "expression.h"
-//#include "expression/constant.h"
-//#include "expression/boolean.h"
-//#include "expression/numerical.h"
+// these includes are used to drive test build of header only expression sources
+#include "expression.h"
+#include "expression/boolean.h"
+#include "expression/conditional.h"
+#include "expression/constant.h"
+
+#include "expression/numerical.h"
 
 using namespace Rcpp;
 using namespace scram::mef;
